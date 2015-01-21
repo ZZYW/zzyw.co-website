92470d7dda0b69f34d1ba1e2592004bcb0cce3d5
nycucumber <yang.wang.dec@gmail.com>
2015-01-21 15:29:30 -0500

    firefox webgl bug fixed

---
+++ static/js/fragmentShader.js
@@ -0,0 +1,13 @@
+
+			uniform vec2 resolution;
+			uniform float time;
+			uniform sampler2D texture;
+
+			void main()	{
+
+				vec2 uv = gl_FragCoord.xy / resolution.xy;
+
+				vec3 color = texture2D( texture, uv ).xyz;
+
+				gl_FragColor = vec4( color, 1.0 );
+			}
